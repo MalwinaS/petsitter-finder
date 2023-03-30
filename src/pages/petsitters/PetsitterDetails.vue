27f65da4afe183b3b51c6b1327ac4d409b366f72
MalwinaS <malwinastepien93@gmail.com>
2023-03-30 16:06:50 +0200

    added petsitter details component

--- src/pages/petsitters/PetsitterDetails.vue
+++ src/pages/petsitters/PetsitterDetails.vue
@@ -1,9 +1,63 @@
 <template>
-    <p>Details for petsitter</p>
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
+      <h3>{{ rate }} PLN/day</h3>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <header>
+        <h2>Interested? Reach out now!</h2>
+        <base-button link :to="contactLink">Contact</base-button>
+      </header>
+    </base-card>
     <router-view></router-view>
-    <router-link to="/petsitters/c1/contact"></router-link>
+  </section>
+  <section>
+    <base-card>
+      <base-badge
+        v-for="area in areas"
+        :key="area"
+        :type="area"
+        :title="area"
+      ></base-badge>
+      <p>{{ description }}</p>
+    </base-card>
+  </section>
 </template>
 
-<script></script>
+<script>
+export default {
+  props: ["id"],
+  data() {
+    return {
+      selectedPetsitter: null,
+    };
+  },
+  computed: {
+    fullName() {
+      return this.selectedPetsitter.firstName + " " + this.selectedPetsitter.lastName;
+    },
+    areas() {
+      return this.selectedPetsitter.areas;
+    },
+    rate() {
+      return this.selectedPetsitter.dailyRate;
+    },
+    description() {
+      return this.selectedPetsitter.description;
+    },
+    contactLink() {
+      return this.$route.path + "/" + this.id + "/contact";
+    },
+  },
+  created() {
+    this.selectedPetsitter = this.$store.getters["petsitters/petsitters"].find(
+      (petsitter) => petsitter.id === this.id
+    );
+  },
+};
+</script>
 
 <style></style>
