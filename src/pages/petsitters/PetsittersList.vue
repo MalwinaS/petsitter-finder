acb0b1f4358513253a429051a9aa84865ef6e563
MalwinaS <malwinastepien93@gmail.com>
2023-03-31 16:25:40 +0200

    added filtering

--- src/pages/petsitters/PetsittersList.vue
+++ src/pages/petsitters/PetsittersList.vue
@@ -1,41 +1,73 @@
 <template>
-  <section>Filter</section>
+  <section>
+    <petsitter-filter @change-filter="setFilters"></petsitter-filter>
+  </section>
+  s
   <section>
     <base-card>
-    <div class="controls">
-      <base-button mode="outline">Refresh</base-button>
-      <base-button link to="/register">Register as Petsitter</base-button>
-    </div>
-    <ul v-if="hasPetsitters">
-      <petsitter-item
-        v-for="petsitter in filteredPetsitters"
-        :key="petsitter.id"
-        :id="petsitter.id"
-        :first-name="petsitter.firstName"
-        :last-name="petsitter.lastName"
-        :rate="petsitter.dailyRate"
-        :areas="petsitter.areas"
-      >
-      </petsitter-item>
-    </ul>
-    <h3 v-else>No petsitters found.</h3>
-  </base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register as Petsitter</base-button>
+      </div>
+      <ul v-if="hasPetsitters">
+        <petsitter-item
+          v-for="petsitter in filteredPetsitters"
+          :key="petsitter.id"
+          :id="petsitter.id"
+          :first-name="petsitter.firstName"
+          :last-name="petsitter.lastName"
+          :rate="petsitter.dailyRate"
+          :areas="petsitter.areas"
+        >
+        </petsitter-item>
+      </ul>
+      <h3 v-else>No petsitters found.</h3>
+    </base-card>
   </section>
 </template>
 
 <script>
 import PetsitterItem from "@/components/petsitters/PetsitterItem.vue";
+import PetsitterFilter from "@/components/petsitters/PetsitterFilter.vue";
 
 export default {
   components: {
     PetsitterItem,
+    PetsitterFilter,
+  },
+  data() {
+    return {
+      activeFilters: {
+        dog: true,
+        cat: true,
+        hamster: true,
+      },
+    };
   },
   computed: {
     filteredPetsitters() {
-      return this.$store.getters["petsitters/petsitters"];
+      const petsitters = this.$store.getters["petsitters/petsitters"];
+      return petsitters.filter((petsitter) => {
+        if (this.activeFilters.dog && petsitter.areas.includes("dog")) {
+          return true;
+        } if (this.activeFilters.cat && petsitter.areas.includes("cat")) {
+          return true;
+        } if (
+          this.activeFilters.hamster &&
+          petsitter.areas.includes("hamster")
+        ) {
+          return true;
+        }
+        return false;
+      });
     },
     hasPetsitters() {
       return this.$store.getters["petsitters/hasPetsitters"];
+    },
+  },
+  methods: {
+    setFilters(updatedFilters) {
+      this.activeFilters = updatedFilters;
     },
   },
 };
