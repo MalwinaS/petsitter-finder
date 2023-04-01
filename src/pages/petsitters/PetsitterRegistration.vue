efe4a6a7814b18e9620417276d1eab26e463bff9
MalwinaS <malwinastepien93@gmail.com>
2023-04-01 15:55:38 +0200

    register form

--- src/pages/petsitters/PetsitterRegistration.vue
+++ src/pages/petsitters/PetsitterRegistration.vue
@@ -1,7 +1,18 @@
 <template>
-  <p>Register</p>
+  <section>
+    <base-card>
+      <h2>Register as a petsitter now!</h2>
+      <petsitter-form></petsitter-form>
+    </base-card>
+  </section>
 </template>
 
-<script></script>
+<script>
+import PetsitterForm from "../../components/petsitters/PetsitterForm.vue";
+
+export default {
+  components: { PetsitterForm },
+};
+</script>
 
 <style></style>
