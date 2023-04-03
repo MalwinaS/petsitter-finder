75d72098800dfc2029c6d6ae582a7c14648c0355
MalwinaS <malwinastepien93@gmail.com>
2023-04-03 09:54:37 +0200

    added contact form

--- src/pages/requests/ContactPetsitter.vue
+++ src/pages/requests/ContactPetsitter.vue
@@ -1,7 +1,87 @@
 <template>
-    <p>Contact</p>
+  <form @submit.prevent="submitForm">
+    <div>
+      <label for="email">Your e-mail</label>
+      <input type="email" id="email" v-model.trim="email" />
+    </div>
+    <div>
+      <label for="message">Message</label>
+      <textarea rows="6" id="message" v-model.trim="message"></textarea>
+    </div>
+    <p class="errors" v-if="!formIsValid">
+      Please enter a valid email and non-empty message
+    </p>
+    <div class="actions">
+      <base-button>Send message</base-button>
+    </div>
+  </form>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      message: "",
+      formIsValid: true,
+    };
+  },
+  methods: {
+    submitForm() {
+      this.formIsValid = true;
+      if (
+        this.email === "" ||
+        !this.email.includes("@") ||
+        this.message === ""
+      ) {
+        this.formIsValid = false;
+        return;
+      }
+    },
+  },
+};
+</script>
 
-<style></style>
+<style scoped>
+form {
+  margin: 1rem;
+  border: 1px solid #f5b44d;
+  border-radius: 10px;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #f5b44d;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea.focus {
+  border-color: #f5b44d;
+  background-color: #f0e6fd;
+  outline: none;
+}
+
+.errors {
+  font-weight: bold;
+  color: red;
+}
+
+.actions {
+  text-align: center;
+}
+</style>
